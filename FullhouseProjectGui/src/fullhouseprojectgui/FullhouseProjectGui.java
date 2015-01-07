4acfabf624aa1b6a190ea7cd10f2c4cbdbbdbdd1
Rayferior <Rayferior@hotmail.com>
2015-01-07 10:17:53 +0100

    Gui V1
    Eerste versie

---
+++ FullhouseProjectGui/src/fullhouseprojectgui/FullhouseProjectGui.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fullhouseprojectgui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Guus
+ */
+public class FullhouseProjectGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         JFrame frame=new Gui();       
+      
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.setVisible(true);
+    }
+}
